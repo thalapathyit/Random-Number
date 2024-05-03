a6ae2a1432f316a9871e37c76c5866053d089b2a
fazilmohammedn <fazilmohammedbca@gmail.com>
2024-05-03 17:02:12 +0530

    update project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// import App from './App';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+function Parent()
+{
+  var [num,setcounter]=useState(0)
+  function changenum()
+  {
+    setcounter(Math.floor(Math.random()*10)+5)
+  }
+  return(
+    <div>
+      <h1>{num}</h1>
+      <button onClick={changenum}>CLICK RANDOM NUMBER</button>
+    </div>
+  )
+}
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ <Parent></Parent>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
